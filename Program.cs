3c9af2acd988b19a5d957ab45a5e5378b6deec92
Olesia <ramisaly7@gmail.com>
2024-05-11 23:33:25 +0300

    Написан код решающий задачу

--- Program.cs
+++ Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-test
+﻿﻿﻿using System;
+class Programm
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите элементы массива через запятую:");
+        string input = Console.ReadLine();
+        string[] FirstArray = input.Split(',');
+        string[] resArray = SortingArray(FirstArray);
+        Console.WriteLine("Новый массив:");
+        foreach (string item in resArray)
+        {
+            Console.Write(item + ",");
+        }
+    }
+    static string[] SortingArray(string[] FirstArray)
+    {
+        int count = 0;
+        foreach (string item in FirstArray)
+        {
+            if (item.Length <= 3)
+            {
+                count++;
+            }
+        }
+        string[] resArray = new string[count];
+        int index = 0;
+        foreach (string item in FirstArray)
+        {
+            if (item.Length <= 3)
+            {
+                resArray[index] = item;
+                index++;
+            }
+        }
+        return resArray;
+    }
+}
